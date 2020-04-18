c8b0baace3487218de927d7435ce6f1f482c4901
Michael Mazur <mazurasdf@gmail.com>
2020-04-18 16:29:15 -0500

    added view and component for projects

---
+++ src/views/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Project from '../components/Project';
+
+const Projects = (props) => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Projects;
